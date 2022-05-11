05e0283b378dbbe7fb2795011fae9f2d28829d26
Nishant <nish.kumbhar02@gmail.com>
2022-05-11 18:59:29 +0530

    Delete E COM directory

--- E COM/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='products-home'),
-    path('about/', views.about, name='products-about'),
-    path('contact/', views.contact, name='products-contact'),
-]
